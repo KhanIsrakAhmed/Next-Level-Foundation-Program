21f7a2994da76c6681eb6a911c25a15b31de47c9
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-01-23 13:07:22 +0600

    totalt mark avg

--- Javascript fundamentals/Module 2/arrayOps.js
+++ Javascript fundamentals/Module 2/arrayOps.js
@@ -1,12 +1,14 @@
-let students ={
-    name : "Israk",
-    id : 101,
+let students = {
+    name: "Israk",
+    id: 101,
+
     marks: {
         bangla: 80,
         english: 90,
         math: 95,
     }
 };
+
 let totalMarks = 0;
 let totalSubjects = 0;
 
@@ -15,6 +17,14 @@ for (const key in students.marks) {
     totalSubjects++;
 }
 
+let avg = totalMarks / totalSubjects;
+
 console.log("Total marks:", totalMarks);
 console.log("Total subjects:", totalSubjects);
-console.log("Average marks:", totalMarks / totalSubjects);
+console.log("Average marks:", avg.toFixed(3));
+
+if (avg >= 80) {
+    console.log("Passed with distinction. Got", avg.toFixed(3), "%");
+} else {
+    console.log("Fail koreche, porashunai focus dite hobe");
+}
