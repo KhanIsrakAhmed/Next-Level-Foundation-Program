728f8332978f4d770b02225ef20fe35275a51dc8
KhanIsrakAhmed <israk.ahmed275@gmail.comAdd-Content README.md>
2026-08-22 22:48:06 +0600

    Create oparetor.js

---
+++ Javascript fundamentals/Module 1/oparetor.js
@@ -0,0 +1,29 @@
+const a = 20;
+const b = 6;
+
+const sum = a + b;
+const difference = a - b;
+const product = a * b;
+const quotient = a / b;
+
+
+console.log("Sum:", sum);
+
+console.log("Difference:", difference);
+
+console.log("Product:", product);
+
+console.log("Quotient:", quotient);
+
+console.log("Remainder:", a % b);
+
+let age = 25;
+
+if (age >= 20) {
+    age ++;
+    console.log("Age", age);
+}
+else if (age >= 18) {
+    console.log("You are an adult.");
+}
+console.log("New Age:", age );
