91cbd10e52765e4ce4ba2a615e5f02e5938f3248
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-08-23 14:06:37 +0600

    Task 02 solved

--- Javascript fundamentals/Module 2/test02.js
+++ Javascript fundamentals/Module 2/test02.js
@@ -4,4 +4,15 @@ let cart = [
   { name: "Shirt", price: 1200, quantity: 2 },
   { name: "Pants", price: 1800, quantity: 1 },
   { name: "Socks", price: 150, quantity: 3 },
-];
+];  
+
+let totalBill = 0;
+let totalItem = 0;
+
+for(let item of cart)
+{
+    totalBill += item.price*item.quantity;
+    totalItem += item.quantity;
+}
+console.log("Total Item brought ", totalItem);
+console.log("Total spends ", totalBill,"BDT")  
