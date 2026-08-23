ea7c1df0d2934c457f1915b5a0a9caa2d6c6a645
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-08-23 12:28:53 +0600

    NEW TASK

---
+++ Javascript fundamentals/Module 2/test02.js
@@ -0,0 +1,7 @@
+// task: koto gulo item kinechen and koto tk bill hoyeche shese BDT te dekhate hobe print kore
+
+let cart = [
+  { name: "Shirt", price: 1200, quantity: 2 },
+  { name: "Pants", price: 1800, quantity: 1 },
+  { name: "Socks", price: 150, quantity: 3 },
+];
