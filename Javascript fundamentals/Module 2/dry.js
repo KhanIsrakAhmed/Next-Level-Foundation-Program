2865d25e5823169f82794ed5f530b242f473dd90
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-08-24 18:27:43 +0600

    update dry.js

--- Javascript fundamentals/Module 2/dry.js
+++ Javascript fundamentals/Module 2/dry.js
@@ -15,3 +15,18 @@ function calculateDiscount(price, discount){
     let discountAmmount = (price * discount)/100;
     return price - discountAmmount;
 }
+
+function finalBill(price,vatPer = 15)
+{
+    let vat = (price * vatPer)/100;
+    return price + vat;
+}
+
+function formatBDT(amount){
+    return `${amount.toFixed(3)} BDT`;
+}
+
+function capitalized(str) {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
