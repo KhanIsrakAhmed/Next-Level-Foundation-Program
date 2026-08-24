f23839a7b087dda609a77cacf0e2c181e464367b
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-08-24 19:07:43 +0600

    update dry.js

--- Javascript fundamentals/Module 2/dry.js
+++ Javascript fundamentals/Module 2/dry.js
@@ -1,32 +1,34 @@
 // we'll build a order processing system to understand dry and how important a function is.
 
-function isValidPrice(price){
-    return typeof price === "number" && price >0;
+function isValidPrice(price) {
+  return typeof price === "number" && price > 0;
 }
 function isValidEmail(email) {
-    return email.includes("@") && email.includes(".");
+  return email.includes("@") && email.includes(".");
 }
 
-
-function calculateDiscount(price, discount){
-    if(!isValidPrice(price)){
-        return 0;
-    }
-    let discountAmmount = (price * discount)/100;
-    return price - discountAmmount;
+function calculateDiscount(price, discount) {
+  if (!isValidPrice(price)) {
+    return 0;
+  }
+  let discountAmmount = (price * discount) / 100;
+  return price - discountAmmount;
 }
 
-function finalBill(price,vatPer = 15)
-{
-    let vat = (price * vatPer)/100;
-    return price + vat;
+function finalBill(price, vatPer = 15) {
+  let vat = (price * vatPer) / 100;
+  return price + vat;
 }
 
-function formatBDT(amount){
-    return `${amount.toFixed(3)} BDT`;
+function formatBDT(amount) {
+  return `${amount.toFixed(3)} BDT`;
 }
 
 function capitalized(str) {
   if (!str) return "";
   return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function processOrder(user, itemPrice, discountCode) {
+  console.log(`---- processing order for ${capitalized(user.name)}----`);
 }
