afb5d0428bd08672c1588fe90edf97e3da92f023
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-08-26 23:26:41 +0600

    task 03 update

--- Javascript fundamentals/Module 2/task03.js
+++ Javascript fundamentals/Module 2/task03.js
@@ -19,11 +19,50 @@ function addStudent(name, age, dept) {
 }
 
 addStudent("Moudud", 31, "BsCivil");
-// console.log(students);
 
 function getAll() {
   for (const student of students) {
     console.log(student);
   }
 }
+
+function findStudents(id) {
+  let found = null;
+
+  for (let student of students) {
+    if (student.id == id) {
+      found = student.name;
+      break;
+    }
+  }
+
+  if (found) {
+    console.log("Found Student:", found);
+  } else {
+    console.log("404 student not found");
+  }
+}
+
+function deleteStudent(id) {
+  let targetStd = null;
+
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].id == id) {
+      targetStd = i;
+      break;
+    }
+  }
+
+  if (targetStd !== null) {
+    let deleteStd = students.splice(targetStd, 1);
+
+    console.log(`Deleted Student: ${deleteStd[0].name} id: ${deleteStd[0].id}`);
+  } else {
+    console.log("404 student not found");
+  }
+}
+
+deleteStudent(276);
 getAll();
+
+findStudents(276);
