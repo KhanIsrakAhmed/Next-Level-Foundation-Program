e25a3f1250f904ac1c9d2bd122523a8062476ac0
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-08-29 13:43:03 +0600

    Update array.js

--- Javascript fundamentals/Module 2/array.js
+++ Javascript fundamentals/Module 2/array.js
@@ -4,3 +4,18 @@ let name =["Israk","Muntasir","Najifa","Sufia"];
 console.log(name);
 
 console.log(name [0]);
+
+name.push("Khan");
+console.log(name);
+
+name.pop();
+console.log(name);
+
+name.unshift("Khan")
+console.log(name)
+
+name.shift()
+console.log(name)
+
+name.splice(1, 2, "Mowdud", "Muntasir");
+console.log(name);
