894789a988d2aa034e987437763986ffb7d382f2
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-08-29 14:18:59 +0600

    Create loop.js

---
+++ Javascript fundamentals/Module 2/loop.js
@@ -0,0 +1,24 @@
+for (let i = 0; i <= 5; i++)
+{
+    console.log(`count: ${i}`);
+}
+
+console.log("now flip");
+
+for (let i = 9; i >= 0; i--)
+{
+    console.log(`count: ${i}`);
+}
+
+let friut = ["apple", "banana", "orange", "mango"];
+for (let i = 0; i < friut.length; i++)
+{
+    console.log(friut[i]);
+}
+
+let i = 0;
+while(i < friut.length)
+{
+    console.log("ekhon khabo ",friut[i]);
+    i++;
+}
