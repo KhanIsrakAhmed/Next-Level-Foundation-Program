b8004f4c4f11151dc46ce9b24b05b919f671a66f
KhanIsrakAhmed <israk.ahmed275@gmail.comAdd-Content README.md>
2026-08-29 13:27:02 +0600

    Update decision.js

--- Javascript fundamentals/Module 1/decision.js
+++ Javascript fundamentals/Module 1/decision.js
@@ -1,10 +1,39 @@
-let age =23
+// let age =23
 
-if (age >= 18)
-{
-    console.log("Voter");
+// if (age >= 18)
+// {
+//     console.log("Voter");
+// }
+// else 
+// {
+//     console.log("Not a voter");
+// }
+let mark = 87;
+
+if (mark >= 90) {
+    console.log("A");
 }
-else 
-{
-    console.log("Not a voter");
+else if (mark >= 86) {
+    console.log("A-");
+}
+else if (mark >= 82) {
+    console.log("B+");
+}
+else if (mark >= 78) {
+    console.log("B");
+}
+else if (mark >= 74) {
+    console.log("B-");
+}
+else if (mark >= 70) {
+    console.log("C+");
+}
+else if (mark >= 66) {
+    console.log("C");
+}
+else if (mark >= 62) {
+    console.log("C-");
+}
+else {
+    console.log("Fail");
 }
