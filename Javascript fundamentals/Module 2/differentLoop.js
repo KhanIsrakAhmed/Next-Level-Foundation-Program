3e9ece0bfceb7c84681fa1b23ccd7e679d666757
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-08-29 14:34:11 +0600

    Update differentLoop.js

--- Javascript fundamentals/Module 2/differentLoop.js
+++ Javascript fundamentals/Module 2/differentLoop.js
@@ -4,14 +4,35 @@
 //     console.log("learning ", tech);
 // }
 
-let obj = {
-    name: "Israk",
-    age: 22,
-    email: "israk@example.com",
-    addess: "Dhaka",
-};
+// let obj = {
+//     name: "Israk",
+//     age: 22,
+//     email: "israk@example.com",
+//     addess: "Dhaka",
+// };
 
-for (const key in obj) 
+// for (const key in obj) 
+//     {
+//         console.log(key,"->", obj[key]);
+//     }
+
+let marks = 50;
+
+for (let i = marks; i <= 100 ; i++)
+{
+    if (i > 80)
     {
-        console.log(key,"->", obj[key]);
+        break;
     }
+    console.log("marks: ", i);
+    i += 5;
+}
+
+for (let i = 0; i <= 10; i++)
+{
+    if (i % 2 != 0)
+    {
+        continue;
+    }
+    console.log("Even number: ", i);
+}
