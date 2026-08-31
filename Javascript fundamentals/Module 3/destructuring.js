b909331f27861030755239a5915daf7cad4d8a79
KhanIsrakAhmed <israk.ahmed275@gmail.com>
2026-08-31 23:02:05 +0600

    Update destructuring.js

--- Javascript fundamentals/Module 3/destructuring.js
+++ Javascript fundamentals/Module 3/destructuring.js
@@ -20,6 +20,15 @@ const student1 = {
     zip: 1206,
   },
 };
+const student2 = {
+  name: "Mowdud",
+  age: 24,
+  address: {
+    city: "Dhaka",
+    zip: 1206,
+  },
+  hobby: ["gurdening", "Gaming"],
+};
 
 const {
   name: studentName,
@@ -27,8 +36,16 @@ const {
 } = student1;
 
 console.log(name, studentName, city, zip);
+const {
+  name: stdName,
+  address: { city: stdCity },
+  hobby: [firstHobby],
+} = student2;
+
+console.log(stdName, stdCity, zip, firstHobby);
 
 const arr = ["first", "second", "third"];
-const [first, second, third] = arr;
+// const [first, second, third] = arr;
+const [, , third] = arr;
 
 console.log(third);
