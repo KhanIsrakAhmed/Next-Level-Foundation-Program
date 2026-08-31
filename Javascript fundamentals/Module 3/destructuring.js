e6d2c711f412ead4b4f3896d86fcd76a96f14daa
KhanIsrakAhmed <israk.ahmed275@gmail.com>
2026-08-31 22:46:43 +0600

    Create destructuring.js

---
+++ Javascript fundamentals/Module 3/destructuring.js
@@ -0,0 +1,29 @@
+const students = {
+  name: "Israk",
+  age: 24,
+  address: "Dhaka",
+};
+
+//old
+const oldNname = students.name;
+
+//new
+const { age, name, address } = students;
+console.log(name);
+console.log(age);
+
+const student1 = {
+  name: "Muntasir",
+  age: 24,
+  address: {
+    city: "Dhaka",
+    zip: 1206,
+  },
+};
+
+const {
+  name: studentName,
+  address: { city, zip },
+} = student1;
+
+console.log(name, studentName, city, zip);
