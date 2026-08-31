cd34ca3dcedf3a98b8caa9535a98b62181746524
KhanIsrakAhmed <israk.ahmed275@gmail.com>
2026-08-31 23:12:11 +0600

    Create spread.js

---
+++ Javascript fundamentals/Module 3/spread.js
@@ -0,0 +1,23 @@
+// Spread Operator - three Dot
+// let newArr = [...oldArr]
+// let newObj = {...oldObj}
+
+let cart = ["fruits", "rice"];
+cart.push("egg");
+
+let cart2 = [...cart, "milk"];
+
+console.log(cart, cart2);
+
+// const info = {
+//   name: "Israk",
+//   age: 24,
+// };
+// const contact = {
+//   email: "israk.ahmed38@gmail.com",
+//   phone: " 015********",
+// };
+
+// const personalInfo = { ...info, ...contact };
+
+// console.log(personalInfo);
