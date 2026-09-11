38294ac54c6e9a0596be86d96b15e567daa62c83
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-09-11 09:30:00 +0600

    Update project documentation

--- Javascript fundamentals/Module 3/arrayOp.js
+++ Javascript fundamentals/Module 3/arrayOp.js
@@ -4,4 +4,16 @@ let newFruits = fruits.forEach((f, idx) => {
   console.log(`${idx + 1} -> ${f}`);
   //   return `${idx + 1} -> ${f}`; foreach can't return
 });
-console.log(newFruits);
+
+let newFruitsMap = fruits.map((f, idx) => {
+  return `${idx + 1} -> ${f}`;
+});
+console.log(newFruitsMap);
+
+let newFruits2 = fruits.map((f) => f.toLowerCase());
+
+console.log(newFruits2, "\n", fruits);
+
+let customFruits = fruits.filter((f) => f.length > 5);
+
+console.log(customFruits);
