8c1d7b8dba51bc74500596ca781328b772947015
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-09-11 10:37:00 +0600

    Update project documentation

--- Javascript fundamentals/Module 3/arrayOp2.js
+++ Javascript fundamentals/Module 3/arrayOp2.js
@@ -1,10 +1,17 @@
-let students = [
-  { name: "Rahim", marks: 85 },
-  { name: "Karim", marks: 45 },
-  { name: "Fahim", marks: 70 },
-];
 let fruits = ["Apple", "Banana", "Mango", "jackfruit"];
 
 //find
 let customFruit = fruits.find((f) => f.length > 5);
 console.log(customFruit);
+
+let findFr = fruits.includes("Mango"); // true false
+console.log(findFr);
+
+let students = [
+  { name: "Rahim", marks: 85 },
+  { name: "Karim", marks: 45 },
+  { name: "Fahim", marks: 70 },
+];
+
+let studentCheck = students.some((s) => s.marks > 80);
+console.log(studentCheck); // true
