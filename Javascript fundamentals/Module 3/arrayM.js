e4c37d248179811895125cfa9035719ebb833b5f
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-09-16 09:40:00 +0600

    Update project documentation

---
+++ Javascript fundamentals/Module 3/arrayM.js
@@ -0,0 +1,6 @@
+let products = [
+  { title: "Mouse", price: 500, inStock: true },
+  { title: "Keyboard", price: 1200, inStock: false },
+  { title: "Monitor", price: 8000, inStock: true },
+  { title: "Headphone", price: 1500, inStock: true },
+];
