ed837f73bbc6b4a65780a04c267b309f25fb0603
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-09-20 11:45:00 +0600

    Update project files

--- Javascript fundamentals/Module 3/safeAccess.js
+++ Javascript fundamentals/Module 3/safeAccess.js
@@ -1,1 +1,15 @@
 // Optional Chaining & Nullish Coalescing (?., ??, Safe Property Access
+let user1 = {
+  name: "Rahim",
+  address: {
+    city: "Dhaka",
+  },
+};
+
+let user2 = {
+  name: "Karim",
+  // no address
+};
+// optional chaining ?
+console.log(user1?.address?.city); // Dhaka
+console.log(user2?.address?.city);
