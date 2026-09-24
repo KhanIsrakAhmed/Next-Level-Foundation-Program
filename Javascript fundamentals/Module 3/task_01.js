faa8bebe1d993f93dcf31a2aa57936c64c0396db
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-09-24 11:25:00 +0600

    Task 01 complete

---
+++ Javascript fundamentals/Module 3/task_01.js
@@ -0,0 +1,14 @@
+let movies = [
+  { title: "Inception", year: 2010, rating: 8.8, genre: "Sci-Fi" },
+  { title: "Titanic", year: 1997, rating: 7.9, genre: "Romance" },
+  { title: "The Matrix", year: 1999, rating: 8.7, genre: "Sci-Fi" },
+  { title: "Avatar", year: 2009, rating: 7.8, genre: "Sci-Fi" },
+  { title: "Notebook", year: 2004, rating: 7.8, genre: "Romance" },
+];
+
+// ১. শুধু Sci-Fi Movie, Rating অনুযায়ী বড় থেকে ছোট সাজানো
+let topSciFi = movies
+  ?.filter((m) => m.genre === "Sci-Fi")
+  ?.sort((a, b) => b.rating - a.rating)
+  ?.map((m) => `${m.title},(${m.rating})`)
+  ?.forEach((m) => console.log(`Dekhbo ki dekhbo na ${m})`));
