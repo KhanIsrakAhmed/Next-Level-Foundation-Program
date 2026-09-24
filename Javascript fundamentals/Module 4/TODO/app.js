e478265b452a4a6c30a93ad08c5baf71fc2b75df
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-09-25 01:25:57 +0600

    Update app.js

--- Javascript fundamentals/Module 4/TODO/app.js
+++ Javascript fundamentals/Module 4/TODO/app.js
@@ -1,1 +1,8 @@
-console.log("todo");
+console.log("To Do Started");
+
+let taskInput = document.getElementById("taskInput");
+let addBtn = document.getElementById("addBtn");
+let taskList = document.getElementById("taskList");
+
+// let addBtnAlt = document.querySelector("#addBtn");
+// console.log(addBtnAlt);
