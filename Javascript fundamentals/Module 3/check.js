3c28b254c18cd087e64d5dcfab5c404a5140dd94
KhanIsrakAhmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2026-09-25 00:39:36 +0600

    Update check.js

--- Javascript fundamentals/Module 3/check.js
+++ Javascript fundamentals/Module 3/check.js
@@ -1,3 +1,13 @@
-import add from "./math.js";
+import { add, sub, mul, div, pi } from "./math.js";
 
 console.log(add(5, 3));
+
+console.log(sub(70, 9));
+
+console.log(sub(70, 9), pi);
+
+console.log(mul(5, 4));
+
+console.log(div(20, 5));
+
+console.log(div(20, 0));
